70c8975b1a1d6732f6c319ec7f13d6f1cbb7a4f6
Greg Santos <gregsantos@gmail.com>
2022-04-17 17:36:38 -0700

    Add FlowFees contract

--- flow/config.js
+++ flow/config.js
@@ -1,7 +1,9 @@
 import {config} from "@onflow/fcl"
 
 config({
+  "env": "testnet",
   "app.detail.title": "Flow Fees",
   "accessNode.api": process.env.NEXT_PUBLIC_ACCESS_NODE_API,
   "discovery.wallet": process.env.NEXT_PUBLIC_DISCOVERY_WALLET,
+  "0xFlowFees": "0x912d5440f7e3769e",
 })
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,39 @@
+import {useState, useEffect} from "react"
 import * as fcl from "@onflow/fcl"
+import {config} from "@onflow/fcl"
 import styles from "../styles/Home.module.css"
 
 const Home = () => {
-  const sendQuery = async () => {
-    const profile = await fcl.query({
-      cadence: `
-        import Profile from 0xProfile
+  const [feeParams, setFeeParams] = useState({feeParams: null})
 
-        pub fun main(address: Address): Profile.ReadOnly? {
-          return Profile.read(address)
-        }
-      `,
-      args: (arg, t) => [arg(user.addr, t.Address)],
+  const getFlowFeesAddress = async () =>
+    (await config.get("env")) === "testnet"
+      ? "0x912d5440f7e3769e"
+      : "0xf919ee77447b7497"
+
+  const prepareGetFlowFeeParametersScript = async () => {
+    const flowFeesAddress = await getFlowFeesAddress()
+
+    return `
+      import FlowFees from ${flowFeesAddress}
+
+      pub fun main(): FlowFees.FeeParameters {
+        return FlowFees.getFeeParameters()
+      }
+    `
+  }
+
+  const getFeeParameters = async () => {
+    const feeParams = await fcl.query({
+      cadence: await prepareGetFlowFeeParametersScript(),
     })
 
-    setName(profile?.name ?? "No Profile")
+    setName(feeParams ?? null)
   }
+
+  useEffect(() => {
+    getFeeParameters()
+  }, [])
 
   return (
     <div className={styles.container}>
