c20ef900a9f4a384576df0fdd53bf5eeaf3f054b
Greg Santos <gregsantos@gmail.com>
2022-04-17 17:39:31 -0700

    Add FlowFees contract address

--- flow/config.js
+++ flow/config.js
@@ -1,9 +1,9 @@
 import {config} from "@onflow/fcl"
 
 config({
-  "env": "testnet",
+  env: "testnet",
   "app.detail.title": "Flow Fees",
   "accessNode.api": process.env.NEXT_PUBLIC_ACCESS_NODE_API,
   "discovery.wallet": process.env.NEXT_PUBLIC_DISCOVERY_WALLET,
-  "0xFlowFees": "0x912d5440f7e3769e",
+  "0xFlowFees": process.env.NEXT_PUBLIC_CONTRACT_FLOW_FEES,
 })
